9c260d31fc0eb78b15477023cce083187153bcd2
andreahuncikova <andhun01@easv365.dk>
2025-11-21 17:02:49 +0100

    footer

--- public_html/includes/footer.php
+++ public_html/includes/footer.php
@@ -1,7 +1,7 @@
-</div>
 <footer class="text-center py-6 text-gray-500 text-sm">
     © <?= date('Y') ?> Hidden Spots — All rights reserved.
 </footer>
+</div>
 
 <!-- Global scripts -->
 <script src="https://cdn.jsdelivr.net/npm/macy@2"></script>
