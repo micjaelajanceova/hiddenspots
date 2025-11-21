f270e65e89ada8d7ca1057d4c82db79f683a2e77
andreahuncikova <andhun01@easv365.dk>
2025-11-21 13:39:19 +0100

    special characters are working now

--- public_html/includes/db.php
+++ public_html/includes/db.php
@@ -9,6 +9,12 @@ $options = [
 
 try {
      $pdo = new PDO($dsn, $user, $pass, $options);
+
+     // set UTF-8 for everything so the database handles special characters correctly
+    $pdo->exec("SET character_set_client = utf8mb4");
+    $pdo->exec("SET character_set_connection = utf8mb4");
+    $pdo->exec("SET character_set_results = utf8mb4");
+    
 } catch (\PDOException $e) {
      throw new \PDOException($e->getMessage(), (int)$e->getCode());
 }
