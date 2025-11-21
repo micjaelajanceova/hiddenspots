67b8777d16d495f43367659c2b9b56c62d2137b1
andreahuncikova <andhun01@easv365.dk>
2025-11-21 19:18:52 +0100

    upload

--- public_html/assets/js/main.js
+++ public_html/assets/js/main.js
@@ -13,26 +13,4 @@ window.addEventListener('load', () => {
   document.getElementById('masonry').style.display = 'block';
 });
 
-// Form submit used in header and index.php
-uploadForm.addEventListener('submit', e => {
-    const lat = latitudeInput.value.trim();
-    const lng = longitudeInput.value.trim();
-    const address = addressInput.value.trim();
 
-    if ((!lat || !lng) && !address) {
-        e.preventDefault();
-        alert('Please select a location either by entering an address or clicking on the map.');
-    }
-});
-
-
-// Background slideshow for login.php
-const slides = document.querySelectorAll('.bg-slide');
-let current = 0;
-slides[current].style.opacity = 1;
-
-setInterval(() => {
-    slides[current].style.opacity = 0;
-    current = (current + 1) % slides.length;
-    slides[current].style.opacity = 1;
-}, 5000); 
