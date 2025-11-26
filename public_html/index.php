34ff1ee54788dfac6e5141de9378bc0d27c8c9f0
micjaelajanceova <micjan01@easv365.dk>
2025-11-26 14:13:45 +0100

    index changes

--- public_html/index.php
+++ public_html/index.php
@@ -59,8 +59,8 @@ try {
             <div class="w-full h-96 bg-gray-200 overflow-hidden">
 
               
-              <small class="absolute top-2 left-2 bg-black/60 text-white text-xs px-2 py-1 rounded">
-                  @<?= htmlspecialchars($s['user_name']) ?>
+            <small class="absolute top-2 left-2 bg-black/60 text-white text-xs px-2 py-1 rounded">
+                  @<?= htmlspecialchars($n['user_name'] ?? 'unknown') ?>
               </small>
 
               <img src="<?=htmlspecialchars($s['file_path'])?>" alt="<?=htmlspecialchars($s['name'])?>" class="w-full h-full object-cover transform transition duration-300 hover:scale-105">
@@ -136,7 +136,7 @@ try {
 
 
             <small class="absolute top-2 left-2 bg-black/60 text-white text-xs px-2 py-1 rounded">
-                  @<?= htmlspecialchars($n['user_name']) ?>
+                  @<?= htmlspecialchars($n['user_name'] ?? 'unknown') ?>
               </small>
 
               <img src="<?=htmlspecialchars($n['file_path'])?>" alt="<?=htmlspecialchars($n['name'])?>" class="w-full h-full object-cover transform transition duration-300 hover:scale-105">
