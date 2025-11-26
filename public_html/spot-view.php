611aad17cc8d469c42b23d2368eb073eb46f3387
micjaelajanceova <micjan01@easv365.dk>
2025-11-26 13:14:13 +0100

    spot-view

--- public_html/spot-view.php
+++ public_html/spot-view.php
@@ -139,15 +139,14 @@ $photo_url = !empty($user['profile_photo']) ? $user['profile_photo'] : null;
 <?php include 'includes/profile-header.php'; ?>
 
 <button id="showCityMapBtn" 
-        class="inline-flex items-center gap-1 w-fit text-blue-600 font-semibold 
-               hover:underline bg-transparent border-0 p-0 cursor-pointer">
+        class="px-3 py-1 bg-black text-white rounded text-sm">
     Show on Map
     <span id="mapArrow" class="inline-block transition-transform duration-300">▼</span>
 </button>
 
 
 <!-- Spot title -->
-<span class="text-gray-500 uppercase block">
+<span class="text-gray-500 block">
     <?= htmlspecialchars($spot['city']) ?>
 </span>
 
