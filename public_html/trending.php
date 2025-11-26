c73bad919801d70b3d0edc818a54dd6e3c79dc27
micjaelajanceova <micjan01@easv365.dk>
2025-11-26 13:32:00 +0100

    added profile-header to trending.php

--- public_html/trending.php
+++ public_html/trending.php
@@ -27,6 +27,9 @@ $trendingAll = $stmtAll->fetchAll(PDO::FETCH_ASSOC);
 ?>
 
 <main class="flex-1 bg-white min-h-screen overflow-y-auto pt-10">
+
+<?php include 'includes/profile-header.php'; ?>
+
   <div class="w-full px-4 sm:px-6 lg:px-8">
 
     <!-- THIS WEEK TRENDING -->
