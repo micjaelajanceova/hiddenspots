6c86ab14097e1cd9cff9bf83c7ef2f5233bd0e0b
andreahuncikova <andhun01@easv365.dk>
2025-11-30 18:46:21 +0100

    respopnsive

--- public_html/includes/profile-header.php
+++ public_html/includes/profile-header.php
@@ -20,7 +20,7 @@ echo "<!-- DEBUG: full path = " . __DIR__ . '/../' . $photo_url . " -->";
 
 <?php if(isset($_SESSION['user_id'])): ?>
   <!-- STICKY PROFILE (only when logged in) -->
-  <div class="fixed top-1 right-0 z-50 px-4 py-3 flex justify-end items-center w-full md:w-auto profile-header">
+  <div class="fixed top-1 right-2 z-50 flex items-center justify-end md:w-auto profile-header">
     <div class="relative">
       <button id="profileBtn" 
   class="flex items-center justify-center w-10 h-10 bg-black text-white rounded-full font-semibold text-lg overflow-hidden 
@@ -43,7 +43,7 @@ echo "<!-- DEBUG: full path = " . __DIR__ . '/../' . $photo_url . " -->";
 
 <?php else: ?>
   <!-- NON-STICKY LOGIN / REGISTER (for guests) -->
-  <div class="absolute top-5 right-5 flex gap-3 z-40">
+  <div class="absolute right-5 flex gap-3 z-40 top-16 md:top-5">
     <a href="auth/login.php?action=login" class="bg-black text-white px-4 py-2 rounded-full">Login</a>
     <a href="auth/login.php?action=register" class="bg-gray-200 text-black px-4 py-2 rounded-full">Register</a>
   </div>
