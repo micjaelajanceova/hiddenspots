f7a3eb94ce1f3b3b1c1e5c82d7ed82e220304ecb
andreahuncikova <andhun01@easv365.dk>
2025-12-07 18:55:34 +0100

    header profile

--- public_html/includes/profile-header.php
+++ public_html/includes/profile-header.php
@@ -17,6 +17,11 @@ if ($user_id) {
 }
 ?>
 
+<?php
+// Hide navbar
+$show_navbar = false;
+include __DIR__ . '/../includes/header.php';
+?>
 <?php if ($session->logged_in()): ?>
   <!-- STICKY PROFILE (only when logged in) -->
   <div class="fixed top-3 right-2 z-50 flex items-center justify-end md:w-auto profile-header">
