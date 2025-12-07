c451c098d44d68a78bcc6bfba6279f61b68424c0
andreahuncikova <andhun01@easv365.dk>
2025-12-07 19:54:18 +0100

    header profile

--- public_html/includes/upload.php
+++ public_html/includes/upload.php
@@ -1,6 +1,6 @@
 <?php
 require_once __DIR__ . '/db.php';
-require_once __DIR__ . '/classes/session.php';
+require_once __DIR__ . '/../classes/session.php';
 $session = new SessionHandle();
 
 if (!$session->getUserId()) {
