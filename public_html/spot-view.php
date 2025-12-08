614f7684b04a35f95f147838c7ec742364c8fe19
andreahuncikova <andhun01@easv365.dk>
2025-12-08 16:14:26 +0100

    spot view

--- public_html/spot-view.php
+++ public_html/spot-view.php
@@ -1,6 +1,6 @@
 <?php
 require_once __DIR__ . '/includes/db.php';
-require_once 'classes/session.php';
+require_once __DIR__ . '/classes/session.php';
 require_once __DIR__ . '/classes/spot.php';
 
 // session start
