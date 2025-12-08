0beab25f386274a4dc96b9893b520503b33e3694
andreahuncikova <andhun01@easv365.dk>
2025-12-08 21:28:50 +0100

    fix favourites profile

--- public_html/favourites.php
+++ public_html/favourites.php
@@ -1,4 +1,6 @@
 <?php
+include 'includes/db.php';
+
 require_once __DIR__ . '/classes/session.php';
 $session = new SessionHandle();
 
@@ -8,7 +10,7 @@ if (!$session->logged_in()) {
     exit();
 }
 
-include 'includes/db.php';
+
 include 'includes/header.php';
 include 'includes/profile-header.php';
 
